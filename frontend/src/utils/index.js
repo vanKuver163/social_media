da186139caf5135df3b5307c7edae360526ec8de
vankuver163 <de1uxe87@mail.ru>
2023-11-02 21:16:17 +0400

    login

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const createOrGetUser = async (response, addUser) => {
+  const base64Url = response.credential.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join('')
+  );
+
+  const { name, picture, sub } = JSON.parse(jsonPayload);
+
+  const user = {
+    _id: sub,
+    _type: 'user',
+    userName: name,
+    image: picture,
+  };
+
+  addUser(user);
+
+  await axios.post(`${BASE_URL}/api/auth`, user);
+};
